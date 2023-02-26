0f7fc3da5e182709c76b83060f923e688481979d
galisaishankar08 <2010030054@klh.edu.in>
2023-02-26 11:02:51 +0530

    Added index.html

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", function(req, res) {
-    res.send("Hello, world!");
-})
+app.use(express.static("frontend"))
 
-app.get("/getmyname", function(req, res) {
-    res.send("SAI SHANKAR");
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/frontend/index.html");
 })
 
 app.listen(3002, function () {
