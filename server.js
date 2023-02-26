a246bee74fd1877f85036dd6c407c026e43b7233
galisaishankar08 <2010030054@klh.edu.in>
2023-02-26 14:24:35 +0530

    added set strict

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/frontend/index.html");
 })
 
+mongoose.set("strictQuery", false)
 mongoose.connect(process.env.MONGO_CONNECTION_STRING, function(err){
     if(err) {
         console.error("Error")
